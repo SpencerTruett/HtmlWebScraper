ff65eafec1a1da86caf19a52b1f9801d1c9120d7
Spencer Truett <srtruett1993@gmail.com>
2020-09-14 11:24:27 -0500

    prints super simple html

--- Scraper.py
+++ Scraper.py
@@ -1,1 +1,11 @@
 # A simple html web scraper
+from urllib.request import urlopen
+
+url = "http://olympus.realpython.org/profiles/aphrodite"
+
+page = urlopen(url)
+
+html_bytes = page.read()
+html = html_bytes.decode("utf-8")
+
+print(html)
